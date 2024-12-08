c9fc6f2ff397154eef0d518bea7036f3053577ac
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 17:17:06 +0100

    Implement widget into Dashboard and correctly handle data

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,9 +1,24 @@
+import Widget from "../components/Widget";
 import useDashboardGetAllData from "../utils/useDashboardGetAllData";
 
 const Dashboard = () => {
-  const { data } = useDashboardGetAllData();
+  const { data, error, isLoading } = useDashboardGetAllData();
 
-  return <div>{data?.name}</div>;
+  if (error) return <p>Oh no! There was an error fetching the data</p>;
+
+  if (isLoading || !data) return <p>Loading...</p>;
+
+  const keyValuesData = data.keyValues;
+  const keyValues = (
+    <div className="flex flex-row h-full w-full space-x-4">
+      {keyValuesData.map((keyValue) => {
+        const { name, value, unit } = keyValue;
+        return <Widget key={name} title={name} value={value} unit={unit} />;
+      })}
+    </div>
+  );
+
+  return <div className="min-h-screen flex flex-wrap p-10">{keyValues}</div>;
 };
 
 export default Dashboard;
