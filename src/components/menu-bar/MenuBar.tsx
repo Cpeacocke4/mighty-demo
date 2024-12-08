3f934dff738e58015abd292dd5816d5af97d85c3
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 15:53:23 +0100

    Pull MenuBar elements out of render

--- src/components/menu-bar/MenuBar.tsx
+++ src/components/menu-bar/MenuBar.tsx
@@ -1,7 +1,37 @@
+import NavItem from "./NavItem";
+
 const MenuBar = () => {
+  const title = (
+    <h1 className="font-heading color-black text-4xl font-medium">Energy</h1>
+  );
+
+  const navItems = [
+    {
+      label: "Dashboard",
+      nav: "/dashboard",
+    },
+    {
+      label: "Sensors",
+      nav: "/sensors",
+    },
+    {
+      label: "Locations",
+      nav: "/locations",
+    },
+  ];
+
+  const nav = (
+    <div className="flex gap-2">
+      {navItems.map((navItem) => (
+        <NavItem label={navItem.label} nav={navItem.nav} />
+      ))}
+    </div>
+  );
   return (
     <nav className="min-w-screen bg-container py-5 px-10 flex flex-wrap items-center justify-between mx-auto">
-      <h1 className="font-heading color-black text-4xl font-medium">Energy</h1>
+      {title}
+
+      {nav}
     </nav>
   );
 };
