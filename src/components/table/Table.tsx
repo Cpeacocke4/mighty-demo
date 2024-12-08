9e60ed2eb20c86f27e9cb7c8f7b6b56feb56fa6d
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 23:24:49 +0100

    Add w-full to Table

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -14,26 +14,28 @@ type TableProps<T> = {
 const Table = <T extends Record<string, unknown>>(props: TableProps<T>) => {
   const { columns, data } = props;
   return (
-    <table className="table-auto bg-container rounded-md shadow-md bd-grey divide-y divide-bg mt-2">
-      <thead>
-        <tr>
-          {columns.map((col, index) => (
-            <th key={index} className="p-5">
-              <p className="font-heading text-sm text-grey font-normal text-left">
-                {col.title && col.title}
-                {!col.title && ""}
-              </p>
-            </th>
-          ))}
-        </tr>
-      </thead>
+    <div>
+      <table className="table-auto w-full bg-container rounded-md shadow-md bd-grey divide-y divide-bg mt-2">
+        <thead>
+          <tr>
+            {columns.map((col, index) => (
+              <th key={index} className="p-5">
+                <p className="font-heading text-sm text-grey font-normal text-left">
+                  {col.title && col.title}
+                  {!col.title && ""}
+                </p>
+              </th>
+            ))}
+          </tr>
+        </thead>
 
-      <tbody className="divide-y divide-bg">
-        {data?.map((i, index) => (
-          <TableRow columns={columns} item={i} key={index} />
-        ))}
-      </tbody>
-    </table>
+        <tbody className="divide-y divide-bg">
+          {data?.map((i, index) => (
+            <TableRow columns={columns} item={i} key={index} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
