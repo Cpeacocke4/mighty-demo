0eb6673411726161bfce933029e9e85f8621267c
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 17:16:31 +0100

    Add styling to NavItem text

--- src/components/menu-bar/NavItem.tsx
+++ src/components/menu-bar/NavItem.tsx
@@ -12,7 +12,11 @@ const NavItem = (props: NavItemProps) => {
   const bgColor = itemActive ? "bg-green" : "";
   return (
     <div className={`${bgColor} rounded-xl px-5`}>
-      <a href={nav} aria-current={itemActive ? "page" : false}>
+      <a
+        href={nav}
+        aria-current={itemActive ? "page" : false}
+        className="font-body font-medium text-black"
+      >
         {label}
       </a>
     </div>
