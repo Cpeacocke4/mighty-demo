ebba041cf530b10e31571208016e07c7422aa750
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 15:47:55 +0100

    Create MenuBar with heading

---
+++ src/components/menu-bar/MenuBar.tsx
@@ -0,0 +1,9 @@
+const MenuBar = () => {
+  return (
+    <nav className="min-w-screen bg-container py-5 px-10 flex flex-wrap items-center justify-between mx-auto">
+      <h1 className="font-heading color-black text-4xl font-medium">Energy</h1>
+    </nav>
+  );
+};
+
+export default MenuBar;
