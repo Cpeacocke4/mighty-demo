f7a7f19dbeb54ba0492805ad3c7ce6dad2e33010
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 15:03:50 +0100

    Add custom base theme and import google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "1280px",
+      lg: "1536x",
+      xl: "1920px",
+    },
+    fontFamily: {
+      heading: ["Syne", "sans-serif"],
+      body: ["Urbanist", "sans-serif"],
+    },
+    colors: {
+      bg: "#EEEEEE",
+      container: "#FFFEFC",
+      black: "#201C25",
+      purple: "#9069B5",
+      green: "#B2F730",
+      grey: "#7C7782",
+    },
     extend: {},
   },
   plugins: [],
