556a771b8ff2ec813a5caa9e124d21d7f4028c1e
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 18:38:55 +0100

    Create SensorColumns

---
+++ src/components/table/SensorColumns.tsx
@@ -0,0 +1,42 @@
+import { SensorType } from "../../types/SensorType";
+import { DataTableColumnType } from "./Table";
+
+type Column = DataTableColumnType<SensorType>;
+
+const columnText = (text: string | number) => (
+  <p className="font-body">{text}</p>
+);
+
+export const sensorId: Column = {
+  title: "ID",
+  render: (sensor) => columnText(sensor.id),
+};
+
+export const address: Column = {
+  title: "Address",
+  render: (sensor) => columnText(sensor.address),
+};
+
+export const placement: Column = {
+  title: "Placement",
+  render: (sensor) => columnText(sensor.placement),
+};
+
+export const sensorStatus: Column = {
+  title: "Status",
+  render: (sensor) => (
+    <div className="bg-green px-5 rounded-xl">
+      <p className="font-heading font-medium text-black">{sensor.status}</p>
+    </div>
+  ),
+};
+
+export const temperature: Column = {
+  title: "Temperature",
+  render: (sensor) => columnText(sensor.temperature),
+};
+
+export const humidity: Column = {
+  title: "Humidity",
+  render: (sensor) => columnText(sensor.humidity),
+};
