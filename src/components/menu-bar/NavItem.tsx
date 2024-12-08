d6ab985600414f3838f40f3861ff04dc1e367cb5
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 00:22:58 +0100

    Add hover styling to NavItem

--- src/components/menu-bar/NavItem.tsx
+++ src/components/menu-bar/NavItem.tsx
@@ -10,12 +10,14 @@ const NavItem = (props: NavItemProps) => {
   const itemActive = active === nav;
 
   const bgColor = itemActive ? "bg-green" : "";
+
+  const hoverState = itemActive ? "" : "hover:text-purple hover:font-bold";
   return (
     <div className={`${bgColor} rounded-xl px-5`}>
       <a
         href={nav}
         aria-current={itemActive ? "page" : false}
-        className="font-body font-medium text-black"
+        className={`font-body font-medium text-black ${hoverState}`}
       >
         {label}
       </a>
