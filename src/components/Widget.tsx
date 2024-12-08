aa3755b21cba0aa2cc09508af9c0364d130ff0c0
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 17:16:42 +0100

    Create Widget component

---
+++ src/components/Widget.tsx
@@ -0,0 +1,24 @@
+type WidgetProps = {
+  title: string;
+  value: number;
+  unit?: string;
+};
+
+const Widget = (props: WidgetProps) => {
+  const { title, value, unit } = props;
+
+  return (
+    <div className="flex grow bg-container px-10 py-6 h-full rounded-md shadow-md">
+      <div className="flex-col space-y-2">
+        <p className="text-black font-body font-regular text-4xl">{`${value}${unit}`}</p>
+        <p className="font-heading text-grey font-medium">{title}</p>
+      </div>
+    </div>
+  );
+};
+
+Widget.defaultProps = {
+  unit: "",
+};
+
+export default Widget;
