3cb4ded813e3c0dc1fbe7f2204cdbd637c3a3b50
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 22:44:07 +0100

    Add default body font to ScreenContainer

--- src/screens/ScreenContainer.tsx
+++ src/screens/ScreenContainer.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 import MenuBar from "../components/menu-bar/MenuBar";
 
 const ScreenContainer = () => (
-  <div className="min-h-screen bg-bg">
+  <div className="min-h-screen bg-bg font-body text-black">
     <MenuBar />
     <Outlet />
   </div>
