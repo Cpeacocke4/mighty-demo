02f41d607dc215b957a96f168ad017850baecb0a
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 23:50:53 +0100

    Integreate sensors table into Sensors

--- src/screens/Sensors.tsx
+++ src/screens/Sensors.tsx
@@ -1,5 +1,42 @@
+import Title from "../components/common/Title";
+import Text from "../components/common/Text";
+import Table from "../components/table/Table";
+import ErrorMsg from "../components/ErrorMsg";
+import Loading from "../components/loading-state/Loading";
+import useDashboardGetAllData from "../utils/useDashboardGetAllData";
+import {
+  address,
+  humidity,
+  placement,
+  sensorId,
+  sensorStatus,
+  temperature,
+} from "../components/table/SensorColumns";
+
 const Sensors = () => {
-  return <p>Sensors</p>;
+  const { data, error, isLoading } = useDashboardGetAllData();
+
+  if (error) return <ErrorMsg />;
+
+  if (isLoading || !data) return <Loading />;
+
+  return (
+    <div className="min-h-screen w-full flex-column p-10 space-y-4">
+      <Title title="Sensors" />
+      <Text text="Track all of your installed sensors" />
+      <Table
+        data={data.indoorSensors}
+        columns={[
+          sensorId,
+          address,
+          placement,
+          sensorStatus,
+          temperature,
+          humidity,
+        ]}
+      />
+    </div>
+  );
 };
 
 export default Sensors;
