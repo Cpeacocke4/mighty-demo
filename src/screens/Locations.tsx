ec463e146b6f9822c7c5ee57f84c389864ec8f88
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 16:17:29 +0100

    Add Sensors and Locations pages

---
+++ src/screens/Locations.tsx
@@ -0,0 +1,5 @@
+const Locations = () => {
+  return <p>Locations</p>;
+};
+
+export default Locations;
---
+++ src/screens/Sensors.tsx
@@ -0,0 +1,5 @@
+const Sensors = () => {
+  return <p>Sensors</p>;
+};
+
+export default Sensors;
