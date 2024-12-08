2fe39c39f593308415e33b0467d6866ccdd3f16c
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 16:28:40 +0100

    Add dashboard, sensors and locations routes to Router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Dashboard from "./screens/Dashboard";
 import ScreenContainer from "./screens/ScreenContainer";
+import Sensors from "./screens/Sensors";
+import Locations from "./screens/Locations";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route element={<ScreenContainer />}>
         <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="sensors" element={<Sensors />} />
+        <Route path="locations" element={<Locations />} />
       </Route>
     </Routes>
   </BrowserRouter>
