478b2d7f25c0504a56a55e9130ce844c72e3fbc4
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-08 16:30:11 +0100

    Add Outlet to ScreenContainer

--- src/screens/ScreenContainer.tsx
+++ src/screens/ScreenContainer.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router";
 import MenuBar from "../components/menu-bar/MenuBar";
 
 const ScreenContainer = () => (
   <div className="min-h-screen bg-bg">
     <MenuBar />
+    <Outlet />
   </div>
 );
 
