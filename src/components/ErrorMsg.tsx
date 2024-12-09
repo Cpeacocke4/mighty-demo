4b6f6582229e2ff8fccdceed12e7f322e874ee5b
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 19:12:42 +0100

    Correct ErrorMsg styling

--- src/components/ErrorMsg.tsx
+++ src/components/ErrorMsg.tsx
@@ -1,8 +1,8 @@
 const ErrorMsg = () => {
   return (
-    <div className="flex h-[75vh] w-auto justify-center items-center">
-      <div className="flex-column self-center">
-        <p className="text-3xl text-errorfont-body">Oh no!</p>
+    <div className="flex h-[75vh] w-auto justify-center">
+      <div className="flex flex-col self-center items-center">
+        <p className="text-3xl text-error font-body">Oh no!</p>
         <p className="text-xl font-body text-black">
           There was an error fetching the data
         </p>
