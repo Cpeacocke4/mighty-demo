ba8a92ee8c3304f10d5ce3860435a7b49c4b6036
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 13:08:02 +0100

    Add horizontal scroll to table

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -14,25 +14,27 @@ type TableProps<T> = {
 const Table = <T extends Record<string, unknown>>(props: TableProps<T>) => {
   const { columns, data } = props;
   return (
-    <table className="table-auto w-full bg-container rounded-md shadow-md bd-grey divide-y divide-bg mt-2">
-      <thead>
-        <tr>
-          {columns.map((col, index) => (
-            <th key={index} className="p-5">
-              <p className="font-heading text-sm text-grey font-normal text-left">
-                {col.title}
-              </p>
-            </th>
-          ))}
-        </tr>
-      </thead>
+    <div className="relative overflow-x-auto w-full">
+      <table className="table-auto w-full bg-container rounded-md shadow-md bd-grey divide-y divide-bg mt-2">
+        <thead>
+          <tr>
+            {columns.map((col, index) => (
+              <th key={index} className="p-5">
+                <p className="font-heading text-sm text-grey font-normal text-left">
+                  {col.title}
+                </p>
+              </th>
+            ))}
+          </tr>
+        </thead>
 
-      <tbody className="divide-y divide-bg">
-        {data.map((i, index) => (
-          <TableRow columns={columns} item={i} key={index} />
-        ))}
-      </tbody>
-    </table>
+        <tbody className="divide-y divide-bg">
+          {data.map((i, index) => (
+            <TableRow columns={columns} item={i} key={index} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
