ace74d9a742a11ab36f2107ea8778336cdfda6f2
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 12:19:05 +0100

    Factor out text-center for items-center in ErrorMsg

--- src/components/ErrorMsg.tsx
+++ src/components/ErrorMsg.tsx
@@ -1,9 +1,9 @@
 const ErrorMsg = () => {
   return (
-    <div className="flex h-[75vh] w-auto justify-center">
+    <div className="flex h-[75vh] w-auto justify-center items-center">
       <div className="flex-column self-center">
-        <p className="text-3xl text-error text-center font-body">Oh no!</p>
-        <p className="text-xl font-body text-black text-center">
+        <p className="text-3xl text-errorfont-body">Oh no!</p>
+        <p className="text-xl font-body text-black">
           There was an error fetching the data
         </p>
       </div>
