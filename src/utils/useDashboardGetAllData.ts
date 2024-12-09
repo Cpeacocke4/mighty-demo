738c5149cc294a804a2d790a25fbe5f90c97fcdd
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 12:54:32 +0100

    Create error toast for bad requests in useDashboardGetAllData

--- src/utils/useDashboardGetAllData.ts
+++ src/utils/useDashboardGetAllData.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import dashboardGetAllData from "./dashboard-get-all-data";
+import toast from "react-hot-toast";
 
 const DASHBOARD_DATA = "DASHBAORD_DATA";
 
@@ -8,6 +9,11 @@ const useDashboardGetAllData = () => {
     queryKey: [DASHBOARD_DATA],
     queryFn: dashboardGetAllData,
   });
+
+  if (query.isError)
+    toast.error(
+      "Oops! There was an error fetching your data, please try again later."
+    );
 
   return query;
 };
