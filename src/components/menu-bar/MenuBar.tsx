7f65394a0d2411b0446230cbf549c46098e53b52
Cpeacocke4 <104768847+Cpeacocke4@users.noreply.github.com>
2024-12-09 11:46:28 +0100

    Correct dashboard route and pull navItems out of MenuBar

--- src/components/menu-bar/MenuBar.tsx
+++ src/components/menu-bar/MenuBar.tsx
@@ -1,48 +1,38 @@
 import { useLocation } from "react-router";
 import NavItem from "./NavItem";
-import { useEffect, useState } from "react";
+
+const navItems = [
+  {
+    label: "Dashboard",
+    nav: "/",
+  },
+  {
+    label: "Sensors",
+    nav: "/sensors",
+  },
+];
 
 const MenuBar = () => {
   const { pathname: path } = useLocation();
-
-  const [activeNav, setActiveNav] = useState("/dashboard");
-
-  useEffect(() => {
-    if (path !== "/") {
-      setActiveNav(path);
-    }
-  }, [path]);
 
   const title = (
     <h1 className="font-heading text-black text-4xl font-medium">Energy</h1>
   );
 
-  const navItems = [
-    {
-      label: "Dashboard",
-      nav: "/dashboard",
-    },
-    {
-      label: "Sensors",
-      nav: "/sensors",
-    },
-  ];
-
-  const nav = (
+  const navButtons = (
     <div className="flex space-x-2">
       {navItems.map((navItem) => {
         const { label, nav } = navItem;
-        return (
-          <NavItem key={label} label={label} nav={nav} active={activeNav} />
-        );
+        return <NavItem key={label} label={label} nav={nav} active={path} />;
       })}
     </div>
   );
+
   return (
     <nav className="min-w-screen bg-container py-5 px-10 flex flex-wrap items-center justify-between mx-auto">
       {title}
 
-      {nav}
+      {navButtons}
     </nav>
   );
 };
